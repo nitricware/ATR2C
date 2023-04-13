9c64c17f7724823d43ffd045216f4e6d351845c1
Kurt Frey <office@nitricware.com>
2023-04-13 18:56:14 +0200

    added a file menu and quit option

--- ATR2C/Program.cs
+++ ATR2C/Program.cs
@@ -3,6 +3,10 @@ using Terminal.Gui;
 Application.Init();
 MenuBar menuBar = new(
     new MenuBarItem[] {
+        new MenuBarItem("File",
+            new MenuItem[] {
+                new MenuItem("Quit", "Quits the application", () => {Application.Shutdown();})
+            }),
         new MenuBarItem("Extras",
             new MenuItem[] {
                 new MenuItem("Digital Contact List...", "", () => {Application.Run<DigitalContactListWindow>();})
